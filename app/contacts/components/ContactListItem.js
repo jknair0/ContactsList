3699e8e8439a7d01c2c04542189695e89bec97c9
Jaya Krishnan Nair <jayakrishnan@sensehawk.com>
2019-07-24 16:38:00 +0530

    UPDATE: changed the class component to functional stateless component

--- app/contacts/components/ContactListItem.js
+++ app/contacts/components/ContactListItem.js
@@ -1,33 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, Icon } from 'native-base';
 import PropTypes from 'prop-types';
 import ContactEntity from '../ContactEntity';
 
 
-export default class ContactListItem extends Component {
-  render() {
-    const { contact, onDeleteClicked } = this.props;
-    const {
-      name, countryCode, phoneNumber,
-    } = contact;
-    return (
-      <View style={styles.root}>
-        <View style={styles.contactContent}>
-          <Text style={styles.contactName}>{name}</Text>
-          <View style={styles.contactNumber}>
-            <Text style={styles.countryCode}>{countryCode}</Text>
-            <Text style={styles.phNumber}>{phoneNumber}</Text>
-          </View>
+export default function ContactListItem(props) {
+  const { contact, onDeleteClicked } = props;
+  const { name, countryCode, phoneNumber } = contact;
+  return (
+    <View style={styles.root}>
+      <View style={styles.contactContent}>
+        <Text style={styles.contactName}>{name}</Text>
+        <View style={styles.contactNumber}>
+          <Text style={styles.countryCode}>{countryCode}</Text>
+          <Text style={styles.phNumber}>{phoneNumber}</Text>
         </View>
-        <Button light onPress={() => onDeleteClicked(contact)}>
-          <Icon name="trash" style={styles.deleteContact}/>
-        </Button>
       </View>
-    );
-  }
+      <Button light onPress={() => onDeleteClicked(contact)}>
+        <Icon name="trash" style={styles.deleteContact}/>
+      </Button>
+    </View>
+  );
 }
-
 
 ContactListItem.propTypes = {
   contact: PropTypes.instanceOf(ContactEntity).isRequired,
